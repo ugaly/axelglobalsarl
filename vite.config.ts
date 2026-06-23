825282fc8911c34d4232c36e48c2537737a1903e
clockwise technology <clockwisetechnology@clockwises-MacBook-Air.local>
2026-06-23 09:56:20 +0300

    sarlu

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
     server: {
       port: 3000,
       host: true,
+      allowedHosts: ["axeglobalsarlu.com", "www.axeglobalsarlu.com", "localhost"],
     },
   },
 });
